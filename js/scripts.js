7b54eb237e18b7bca950019461cb1ad64429a925
Janice Dunn & Vaughn Dice <student@epicodus.com>
2015-09-14 14:37:57 -0700

    add ability to assign tasks to a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
-var lists = [];
+var lists = {'exampleList' : {name: 'exampleList', tasks: []} };
 
 $(document).ready(function() {
-
   showLists();
+  for (var listName in lists) {
+    $('ul#lists').append('<li><span class="list">' + listName + "</span></li>")
+  }
 
   $("form#new-task").submit(function(event) {
     event.preventDefault();
 
     var newTaskDescription = $("input#new-task-description").val();
-    var newTask = { description: newTaskDescription };
+    var newTaskList = $("select#list-select").val();
+    var newTask = { description: newTaskDescription, list: newTaskList };
+    lists[newTaskList].tasks.push(newTask);
 
     $('ul#tasks').append('<li><span class="task">' + newTask.description + "</span></li>")
 
     $(".task").last().click(function() {
       $("#show-task").fadeIn();
       $("#show-task h2").text(newTask.description);
-      $(".task-description").text(newTask.description);
+      $(".assigned-list").text(newTask.list);
     });
 
     $("input#new-task-description").val("");
@@ -25,8 +29,8 @@ $(document).ready(function() {
     event.preventDefault();
 
     var newListName = $("input#new-list-name").val();
-    var newList = { name: newListName };
-    lists.push(newList);
+    var newList = { name: newListName, tasks: [] };
+    lists[newList.name] = newList;
     showLists();
 
     $('ul#lists').append('<li><span class="list">' + newList.name + "</span></li>")
@@ -34,7 +38,10 @@ $(document).ready(function() {
     $(".list").last().click(function() {
       $("#show-list").fadeIn();
       $("#show-list h2").text(newList.name);
-      $(".list-name").text(newList.name);
+      newList.tasks.forEach(function(task) {
+        console.log(task.description)
+        $(".list-tasks").text(task.description);
+      });
     });
 
     $("input#new-list-name").val("");
@@ -42,12 +49,8 @@ $(document).ready(function() {
 });
 
 var showLists = function() {
-  lists.forEach(function(list) {
-    $("#checkboxes").append('<div class="checkbox">' +
-                              '<label>' +
-                                '<input type="checkbox" name="lists[]">' +
-                                  list.name +
-                              '</label>' +
-                            '</div>');
-  });
+  $("#list-select").html("");
+  for (var listName in lists) {
+    $("#list-select").append('<option value="' + listName + '">' + listName + '</option>');
+  }
 }
