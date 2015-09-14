a3a943ca4d479d70ba4a529c1b21b02c421c605f
Janice Dunn & Vaughn Dice <student@epicodus.com>
2015-09-14 12:25:46 -0700

    add showLists function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
+var lists = [];
+
 $(document).ready(function() {
+
+  showLists();
+
   $("form#new-task").submit(function(event) {
     event.preventDefault();
 
     var newTaskDescription = $("input#new-task-description").val();
-    var newTask = { description: newTaskDescription, list:  };
+    var newTask = { description: newTaskDescription };
 
     $('ul#tasks').append('<li><span class="task">' + newTask.description + "</span></li>")
 
@@ -21,6 +26,8 @@ $(document).ready(function() {
 
     var newListName = $("input#new-list-name").val();
     var newList = { name: newListName };
+    lists.push(newList);
+    showLists();
 
     $('ul#lists').append('<li><span class="list">' + newList.name + "</span></li>")
 
@@ -33,3 +40,14 @@ $(document).ready(function() {
     $("input#new-list-name").val("");
   });
 });
+
+var showLists = function() {
+  lists.forEach(function(list) {
+    $("#checkboxes").append('<div class="checkbox">' +
+                              '<label>' +
+                                '<input type="checkbox" name="lists[]">' +
+                                  list.name +
+                              '</label>' +
+                            '</div>');
+  });
+}
